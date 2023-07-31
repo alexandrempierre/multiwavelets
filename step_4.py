b77356e0c34cc1dfdab0d1c5ccc8c67caef432cd
Alexandre Pierre <alexandrempierre@gmail.com>
2023-07-30 23:53:21 -0300

    step_4 feito e testado

--- step_4.py
+++ step_4.py
@@ -3,23 +3,55 @@
 
 
 __all__ = [
-    'operator_matrix', 'operator_matrix_extracts', 'extract_params',
-    'extract_submatrices', 'is_zero_or_power_of_2',
+    'operator_matrix', 'extract_submatrices', 'is_zero_or_power_of_2',
+    'Submatrix',
 ]
 __author__ = 'Alexandre Pierre'
 __email__ = 'alexandrempierre [at] gmail [dot] com'
 
 
-from collections.abc import Callable, Iterator
+import functools as ft
+import operator as op
+#
+from collections.abc import Callable
+from dataclasses import dataclass
 #
 import numpy as np
 import numpy.typing as npt
 
 
+NDArrayFloat = npt.NDArray[np.float64]
+
+
+@dataclass
+class Submatrix:
+    '''submatriz de operador a ser aproximada por uma matriz de posto k'''
+    subarray: NDArrayFloat
+    row_start: int
+    col_start: int
+
+    def __eq__(self, s: 'Submatrix') -> bool:
+        # pylint: disable=invalid-name
+        return (
+            self.row_start == s.row_start
+            and self.col_start == s.col_start
+            and np.all(self.subarray == s.subarray)
+        )
+
+    def __hash__(self) -> int:
+        astuple = tuple(
+            (row, ) if isinstance(row, np.number) else tuple(row)
+            for row in self.subarray
+        )
+        return hash(
+            (astuple, self.row_start, self.col_start)
+        )
+
+
 def operator_matrix(
-    xs: npt.NDArray[float],
+    xs: NDArrayFloat,
     kernel_fn: Callable[[float, float], float],
-) -> npt.NDArray[float]:
+) -> NDArrayFloat:
     # pylint: disable=invalid-name
     '''matriz do operador usando método de quadratura do trapézio'''
     n = xs.shape
@@ -32,55 +64,52 @@ def operator_matrix(
     ])
 
 
-def operator_matrix_extracts(
-    T: npt.NDArray[float],
-    k: int
-) -> list[npt.NDArray[float]]:
-    # pylint: disable=invalid-name
-    '''extrair submatrizes'''
-    n = T.shape[0]
-    extracts = {}
-    for start_idx, sub_size, jmp_odd in extract_params(n, k):
-        extracts[sub_size] = (
-            extracts.get(sub_size, [])
-            + [extract_submatrices(T, start_idx, sub_size, jmp_odd)]
-        )
-    return [sum(Vs) for Vs in extracts.values()]
-
-
-def extract_params(n: int, k: int) -> Iterator[tuple[int, int, bool]]:
-    # pylint: disable=invalid-name
-    '''gerar parâmetros (índice inicial $start_idx, tamanho da submatriz
-    quadrada $sub_size e se índices ímpares devem ser pulados $jmp_odd) para
-    extrair submatrizes
-'''
-    start_idx, sub_size = 0, k
-    while start_idx < n:
-        jmp_odd = is_zero_or_power_of_2(start_idx // k)
-        yield start_idx, sub_size, jmp_odd
-        start_idx += sub_size
-        if jmp_odd:
-            sub_size *= 2
-
-
 def extract_submatrices(
-    T: npt.NDArray[float],
-    start_idx: int,
-    sub_size: int,
-    jmp_odd: bool,
-) ->  npt.NDArray[float]:
+    T: NDArrayFloat, k: int,
+    r0: int = 0, c0: int = 0,
+    size: int | None = None,
+) -> set[Submatrix]:
     # pylint: disable=invalid-name
     '''extrai submatrizes'''
-    n = T.shape[0]
-    X = np.zeros_like(T)
-    for step, idx in enumerate(range(start_idx, n, sub_size)):
-        end = idx + sub_size
-        idx_orth = step * sub_size
-        end_orth = idx_orth + sub_size
-        if step % 2 == 0 or not jmp_odd:
-            X[idx:end, idx_orth:end_orth] = T[idx:end, idx_orth:end_orth]
-            X[idx_orth:end_orth, idx:end] = T[idx_orth:end_orth, idx:end]
-    return X
+    if size is None:
+        size = T.shape[0]
+    if size == k:
+        return {Submatrix(T[r0:r0 + k, c0:c0 + k], r0, c0)}
+    if size == 4*k:
+        return {
+            Submatrix(
+                T[
+                    r0 + rows_step:r0 + rows_step + k,
+                    c0 + cols_step:c0 + cols_step + k
+                ],
+                r0 + rows_step, c0 + cols_step
+            )
+            for rows_step in range(0, 4*k, k)
+            for cols_step in range(0, 4*k, k)
+        }
+    s = (0, size//4, size//2, 3*size//4, size)
+    return ft.reduce(
+        op.or_, [
+            extract_submatrices(T, k, r0, c0, size // 2),
+            extract_submatrices(T, k, r0 + s[1], c0 + s[1], size // 2),
+            extract_submatrices(T, k, r0 + s[2], c0 + s[2], size // 2),
+        ], set()
+    ) | {
+        Submatrix(T[r0:r0 + s[1], c0 + s[2]:c0 + s[3]], r0, c0 + s[2]),
+        Submatrix(T[r0:r0 + s[1], c0 + s[3]:c0 + s[4]], r0, c0 + s[3]),
+        Submatrix(
+            T[r0 + s[1]:r0 + s[2], c0 + s[3]:c0 + s[4]],
+            r0 + s[1],
+            c0 + s[3],
+        ),
+        Submatrix(T[r0 + s[2]:r0 + s[3], c0:c0 + s[1]], r0 + s[2], c0),
+        Submatrix(T[r0 + s[3]:r0 + s[4], c0:c0 + s[1]], r0 + s[3], c0),
+        Submatrix(
+            T[r0 + s[3]:r0 + s[4], c0 + s[1]:c0 + s[2]],
+            r0 + s[3],
+            c0 + s[1],
+        ),
+    }
 
 
 def is_zero_or_power_of_2(value: int) -> bool:
--- tests/step_4.py
+++ tests/step_4.py
@@ -6,6 +6,8 @@ __author__ = 'Alexandre Pierre'
 __email__ = 'alexandrempierre [at] gmail [dot] com'
 
 
+import numpy as np
+#
 from .. import step_4
 
 
@@ -22,9 +24,146 @@ esperados
             assert not step_4.is_zero_or_power_of_2(notpow2)
 
 
-class TestExtraction:
+class TestSubmatrixExtraction:
     # pylint: disable=invalid-name, missing-function-docstring
     '''testes para conferir se o processo de extrar as submatrizes está correto
 '''
-    def test_(self):
-        pass
+
+    def test_n_4_k_1_submatrix_extraction(self):
+        # n = 4
+        k = 1
+        matrix = np.array([
+            [11, 12, 13, 14],
+            [21, 22, 23, 24],
+            [31, 32, 33, 34],
+            [41, 42, 43, 44],
+        ])
+        submatrices = {
+            step_4.Submatrix(
+                subarray=np.array([val]),
+                row_start=row_idx,
+                col_start=col_idx,
+            )
+            for row_idx, row in enumerate(matrix)
+            for col_idx, val in enumerate(row)
+        }
+        extracted_submatrices = step_4.extract_submatrices(matrix, k)
+        assert submatrices == extracted_submatrices
+
+    def test_n_8_k_2_submatrix_extraction(self):
+        n = 8
+        k = 2
+        matrix = np.array([
+            [11, 12, 13, 14, 15, 16, 17, 18],
+            [21, 22, 23, 24, 25, 26, 27, 28],
+            [31, 32, 33, 34, 35, 36, 37, 38],
+            [41, 42, 43, 44, 45, 46, 47, 48],
+            [51, 52, 53, 54, 55, 56, 57, 58],
+            [61, 62, 63, 64, 65, 66, 67, 68],
+            [71, 72, 73, 74, 75, 76, 77, 78],
+            [81, 82, 83, 84, 85, 86, 87, 88],
+        ])
+        submatrices = {
+            step_4.Submatrix(
+                subarray=matrix[row:row+k, col:col+k],
+                row_start=row, col_start=col
+            )
+            for row in range(0, n, k) for col in range(0, n, k)
+        }
+        extracted_submatrices = step_4.extract_submatrices(matrix, k)
+        assert submatrices == extracted_submatrices
+
+
+    def test_n_8_k_1_submatrix_extraction(self):
+        # n = 8
+        k = 1
+        matrix = np.array([
+            [11, 12, 13, 14, 15, 16, 17, 18],
+            [21, 22, 23, 24, 25, 26, 27, 28],
+            [31, 32, 33, 34, 35, 36, 37, 38],
+            [41, 42, 43, 44, 45, 46, 47, 48],
+            [51, 52, 53, 54, 55, 56, 57, 58],
+            [61, 62, 63, 64, 65, 66, 67, 68],
+            [71, 72, 73, 74, 75, 76, 77, 78],
+            [81, 82, 83, 84, 85, 86, 87, 88],
+        ])
+        submatrices = {
+            step_4.Submatrix(np.array([10 * (row + 1) + col + 1]), row, col)
+            for col in range(4) for row in range(4)
+        } | {
+            step_4.Submatrix(
+                np.array([40 + 10 * (row + 1) + col + 5]),
+                row + 4,
+                col + 4
+            )
+            for col in range(4) for row in range(4)
+        } | {
+            step_4.Submatrix(
+                subarray=np.array([35]), row_start=2, col_start=4
+            ),
+            step_4.Submatrix(
+                subarray=np.array([45]), row_start=3, col_start=4
+            ),
+            step_4.Submatrix(
+                subarray=np.array([36]), row_start=2, col_start=5
+            ),
+            step_4.Submatrix(
+                subarray=np.array([46]), row_start=3, col_start=5
+            ),
+            step_4.Submatrix(
+                subarray=np.array([53]), row_start=4, col_start=2
+            ),
+            step_4.Submatrix(
+                subarray=np.array([54]), row_start=4, col_start=3
+            ),
+            step_4.Submatrix(
+                subarray=np.array([63]), row_start=5, col_start=2
+            ),
+            step_4.Submatrix(
+                subarray=np.array([64]), row_start=5, col_start=3
+            ),
+            step_4.Submatrix(
+                subarray=np.array([
+                    [15, 16],
+                    [25, 26]
+                ]),
+                row_start=0, col_start=4
+            ),
+            step_4.Submatrix(
+                subarray=np.array([
+                    [17, 18],
+                    [27, 28]
+                ]),
+                row_start=0, col_start=6
+            ),
+            step_4.Submatrix(
+                subarray=np.array([
+                    [37, 38],
+                    [47, 48]
+                ]),
+                row_start=2, col_start=6
+            ),
+            step_4.Submatrix(
+                subarray=np.array([
+                    [51, 52],
+                    [61, 62]
+                ]),
+                row_start=4, col_start=0
+            ),
+            step_4.Submatrix(
+                subarray=np.array([
+                    [71, 72],
+                    [81, 82]
+                ]),
+                row_start=6, col_start=0
+            ),
+            step_4.Submatrix(
+                subarray=np.array([
+                    [73, 74],
+                    [83, 84]
+                ]),
+                row_start=6, col_start=2
+            ),
+        }
+        extracted_submatrices = step_4.extract_submatrices(matrix, k)
+        assert submatrices == extracted_submatrices
