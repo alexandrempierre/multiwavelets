0bef9f6d47fb8ea4a3ab69ef40cd250008981849
Alexandre Pierre <alexandrempierre@gmail.com>
2023-10-10 13:31:44 -0300

    testes do passo 3 consertados

--- tests/step_3.py
+++ tests/step_3.py
@@ -28,25 +28,46 @@ class Test_S1_S2:
     '''_'''
     def test_shift_scale_matrices(self):
         n, k = 8, 2
-        xs = np.linspace(0, 1, n)
+        xs = np.linspace(0, 1, n, endpoint=True)
         mus = step_0.mu_list(xs, k)
         sigmas = step_0.sigma_list(xs, k)
         matrices = next(step_3.shift_scale_matrices(n, k, mus, sigmas), None)
         assert matrices is not None
         S1, S2 = matrices
-        assert np.all(
-            S1 == np.array([
-                [1, -14/9,  784/81, -21952/729],
-                [0,   7/3, -392/27,    5488/81],
-                [0,     0,    49/9,   -1372/27],
-                [0,     0,       0,     343/27],
-            ])
-        )
-        assert np.all(
-            S2 == np.array([
-                [1, 4/7, 16/49, 64/323],
-                [0,   1,   8/7,  48/49],
-                [0,   0,     1,   12/7],
-                [0,   0,     0,      1],
-            ])
-        )
+        S1_test = np.array([
+            [1, -4/7,  16/49,  -64/343],
+            [0,  3/7, -24/49,  144/343],
+            [0,    0,   9/49, -108/343],
+            [0,    0,      0,   27/343],
+        ])
+        S2_test = np.array([
+            [1,  4/7,  16/49,   64/343],
+            [0,  3/7,  24/49,  144/343],
+            [0,    0,   9/49,  108/343],
+            [0,    0,      0,   27/343],
+        ])
+        assert np.allclose(S1, S1_test, rtol=1e-6, atol=1e-8)
+        assert np.allclose(S2, S2_test, rtol=1e-6, atol=1e-8)
+        # for S1_row, S1_test_row in zip(S1, S1_test):
+        #     for S1_elem, S1_test_elem in zip(S1_row, S1_test_row):
+        #         assert np.abs(S1_elem - S1_test_elem) < 1e-3
+        # for S22_row, S2_test_row in zip(S2, S2_test):
+        #     for S2_elem, S2_test_elem in zip(S2_row, S2_test_row):
+        #         assert np.abs(S2_elem - S2_test_elem) < 1e-3
+        #
+        # assert np.all(
+        #     S1 == np.array([
+        #         [1, -14/9,  784/81, -21952/729],
+        #         [0,   7/3, -392/27,    5488/81],
+        #         [0,     0,    49/9,   -1372/27],
+        #         [0,     0,       0,     343/27],
+        #     ])
+        # )
+        # assert np.all(
+        #     S2 == np.array([
+        #         [1, 4/7, 16/49, 64/323],
+        #         [0,   1,   8/7,  48/49],
+        #         [0,   0,     1,   12/7],
+        #         [0,   0,     0,      1],
+        #     ])
+        # )
